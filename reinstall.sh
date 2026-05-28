d7a6b350990cf79da10c1bcebbaf752e2896f3ea
zhuhui1028 <228252012@qq.com>
2026-05-29 01:04:48 +0800

    Update reinstall.sh

--- reinstall.sh
+++ reinstall.sh
@@ -3,9 +3,9 @@
 # shellcheck disable=SC2086
 
 set -eE
-confhome=https://raw.githubusercontent.com/bin456789/reinstall/main
-confhome_cn=https://cnb.cool/bin456789/reinstall/-/git/raw/main
-# confhome_cn=https://www.ghproxy.cc/https://raw.githubusercontent.com/bin456789/reinstall/main
+confhome=https://raw.githubusercontent.com/zhuhui1028/reinstall/main
+confhome_cn=https://cnb.cool/zhuhui1028/reinstall/-/git/raw/main
+# confhome_cn=https://www.ghproxy.cc/https://raw.githubusercontent.com/zhuhui1028/reinstall/main
 
 # 默认密码
 DEFAULT_PASSWORD=123@@@
@@ -81,7 +81,7 @@ Usage: $reinstall_____ anolis      7|8|23
                        [--rdp-port   PORT]
                        [--add-driver INF_OR_DIR]
 
-Manual: https://github.com/bin456789/reinstall
+Manual: https://github.com/zhuhui1028/reinstall
 
 EOF
     exit 1
